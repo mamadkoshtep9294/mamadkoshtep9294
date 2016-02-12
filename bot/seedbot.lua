c279f802ef501caf0b01f91e0cb7f957ea8babce
mamadkoshtep9294 <m.moomeneh2015@gmail.com>
2016-02-12 14:57:33 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,8 +226,8 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
-    disabled_channels = {},
+    sudo_users = {126932779},--Sudo users
+    disabled_channels = {@mamadreza9294},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
